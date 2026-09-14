b9250deaa125d475096b4dd59565af8b0736cfc0
Raazarya12 <125205649+Raazarya12@users.noreply.github.com>
2026-09-14 22:10:02 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -15,7 +15,10 @@ async def render_page(id, secure_hash):
         logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
         logging.debug(f"Invalid hash for message with - ID {id}")
         raise InvalidHash
-    src = urllib.parse.urljoin(Var.URL, f'{secure_hash}{str(id)}')
+    src = urllib.parse.urljoin(
+    Var.URL,
+    f'{secure_hash}{str(id)}?hash={secure_hash}&stream=1'
+)
     if str(file_data.mime_type.split('/')[0].strip()) == 'video':
         async with aiofiles.open('Adarsh/template/req.html') as r:
             heading = 'Watch {}'.format(file_data.file_name)
