52a2a2165121cf97aa84a3785d228f1bbf24cac1
Raazarya12 <125205649+Raazarya12@users.noreply.github.com>
2026-09-14 22:26:13 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -3,6 +3,7 @@ from Adarsh.bot import StreamBot
 from Adarsh.utils.human_readable import humanbytes
 from Adarsh.utils.file_properties import get_file_ids
 from Adarsh.server.exceptions import InvalidHash
+
 import urllib.parse
 import aiofiles
 import logging
@@ -10,30 +11,118 @@ import aiohttp
 
 
 async def render_page(id, secure_hash):
-    file_data=await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
+    file_data = await get_file_ids(
+        StreamBot,
+        int(Var.BIN_CHANNEL),
+        int(id)
+    )
+
     if file_data.unique_id[:6] != secure_hash:
-        logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
-        logging.debug(f"Invalid hash for message with - ID {id}")
+        logging.debug(
+            f'link hash: {secure_hash} - {file_data.unique_id[:6]}'
+        )
+        logging.debug(
+            f"Invalid hash for message with - ID {id}"
+        )
         raise InvalidHash
+
+    # WATCH / STREAM URL
     src = urllib.parse.urljoin(
-    Var.URL,
-    f'{secure_hash}{str(id)}?hash={secure_hash}&stream=1'
-)
+        Var.URL,
+        f'{secure_hash}{str(id)}?hash={secure_hash}&stream=1'
+    )
+
+    # VIDEO
     if str(file_data.mime_type.split('/')[0].strip()) == 'video':
-        async with aiofiles.open('Adarsh/template/req.html') as r:
-            heading = 'Watch {}'.format(file_data.file_name)
+
+        async with aiofiles.open(
+            'Adarsh/template/req.html'
+        ) as r:
+
+            heading = 'Watch {}'.format(
+                file_data.file_name
+            )
+
             tag = file_data.mime_type.split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
+
+            html = (
+                await r.read()
+            ).replace(
+                'tag',
+                tag
+            ) % (
+                heading,
+                file_data.file_name,
+                src
+            )
+
+    # AUDIO
     elif str(file_data.mime_type.split('/')[0].strip()) == 'audio':
-        async with aiofiles.open('Adarsh/template/req.html') as r:
-            heading = 'Listen {}'.format(file_data.file_name)
+
+        async with aiofiles.open(
+            'Adarsh/template/req.html'
+        ) as r:
+
+            heading = 'Listen {}'.format(
+                file_data.file_name
+            )
+
             tag = file_data.mime_type.split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
+
+            html = (
+                await r.read()
+            ).replace(
+                'tag',
+                tag
+            ) % (
+                heading,
+                file_data.file_name,
+                src
+            )
+
+    # OTHER FILES = DOWNLOAD
     else:
-        async with aiofiles.open('Adarsh/template/dl.html') as r:
+
+        # Normal download URL
+        download_src = urllib.parse.urljoin(
+            Var.URL,
+            f'{secure_hash}{str(id)}?hash={secure_hash}'
+        )
+
+        async with aiofiles.open(
+            'Adarsh/template/dl.html'
+        ) as r:
+
             async with aiohttp.ClientSession() as s:
-                async with s.get(src) as u:
-                    heading = 'Download {}'.format(file_data.file_name)
-                    file_size = humanbytes(int(u.headers.get('Content-Length')))
-                    html = (await r.read()) % (heading, file_data.file_name, src, file_size)
+
+                async with s.get(
+                    download_src
+                ) as u:
+
+                    heading = 'Download {}'.format(
+                        file_data.file_name
+                    )
+
+                    content_length = u.headers.get(
+                        'Content-Length'
+                    )
+
+                    if content_length:
+                        file_size = humanbytes(
+                            int(content_length)
+                        )
+                    else:
+                        file_size = humanbytes(
+                            int(file_data.file_size)
+                        )
+
+                    html = (
+                        await r.read()
+                    ) % (
+                        heading,
+                        file_data.file_name,
+                        download_src,
+                        file_size
+                    )
+
     return html
